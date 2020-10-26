5df11978834e40385f67638a9050b106a0ac57fc
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-26 15:34:38 -0500

    feat: create albums_migration.sql

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE IF NOT EXISTS albums(
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist CHAR(50) NOT NULL,
+name CHAR(100) NOT NULL,
+release_date DATE,
+sales DOUBLE,
+genre CHAR(150) NOT NULL,
+PRIMARY KEY (id)
+)

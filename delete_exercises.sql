c2861072715cda00033405408a9435534a1d81ab
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-27 14:15:21 -0500

    feat: refactor SELECT's to DELETE and add SELECT on line 10 to show the deleted albums are removed from the list

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,8 +1,10 @@
 USE codeup_test_db;
 
 SELECT 'RELEASE DATE AFTER 1991' AS 'ALBUMS WITH A';
-SELECT * FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 SELECT 'THE DISCO GENRE' AS 'ALBUMS WITH';
-SELECT * FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE genre = 'disco';
 SELECT 'ARTIST "Various artists"' AS 'ALBUMS BY THE';
-SELECT * FROM albums WHERE artist = 'Various artists';
+DELETE FROM albums WHERE artist = 'Various artists';
+
+SELECT * FROM albums;

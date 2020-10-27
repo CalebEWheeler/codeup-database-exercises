bdb1c44c503ce09996b57edbf3fd011cfa4c1e02
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-27 13:26:54 -0500

    feat: add comments that will tell me what to UPDATE

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,10 +1,13 @@
 USE codeup_test_db;
 
+-- MAKE ALL ALBUMS 10X MORE POPULAR(sales * 10) --
 -- SELECT 'ALL ALBUMS' AS 'IN TABLE';
 -- SELECT name FROM albums;
 
+-- MOVE ALL ALBUMS BEFORE 1980 BACK TO 1800'S --
 -- SELECT 'ALBUMS RELEASED' AS 'BEFORE 1980';
 -- SELECT name FROM albums WHERE release_date < 1980;
 
-SELECT 'MICHAEL JACKSON' AS 'ALBUM LIST';
-SELECT name FROM albums WHERE artist = 'Michael Jackson';
+-- CHANGE 'Michael Jackson' to 'Peter Jackson' --
+-- SELECT 'MICHAEL JACKSON' AS 'ALBUM LIST';
+-- SELECT name FROM albums WHERE artist = 'Michael Jackson';

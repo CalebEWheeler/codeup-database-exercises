67557c288005632109072d7bdb795fe9992d1ee5
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-27 13:24:07 -0500

    feat: create SELECT that displays all albums by Michael Jackson

--- update_exercises.sql
+++ update_exercises.sql
@@ -3,5 +3,8 @@ USE codeup_test_db;
 -- SELECT 'ALL ALBUMS' AS 'IN TABLE';
 -- SELECT name FROM albums;
 
-SELECT 'ALBUMS RELEASED' AS 'BEFORE 1980';
-SELECT name FROM albums WHERE release_date < 1980;
+-- SELECT 'ALBUMS RELEASED' AS 'BEFORE 1980';
+-- SELECT name FROM albums WHERE release_date < 1980;
+
+SELECT 'MICHAEL JACKSON' AS 'ALBUM LIST';
+SELECT name FROM albums WHERE artist = 'Michael Jackson';

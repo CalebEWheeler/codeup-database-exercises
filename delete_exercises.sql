51e5c0db6cf6545ac48a63fbcbdf384bbd16ed5c
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-27 14:12:00 -0500

    feat: refactor SELECT * FROM albums WHERE artist = 'Whitney Houston' to 'Various artists', and add captions before each SELECT

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,5 +1,8 @@
 USE codeup_test_db;
 
--- SELECT * FROM albums WHERE release_date > 1991;
--- SELECT * FROM albums WHERE genre = 'disco';
-SELECT * FROM albums WHERE artist = 'James Horner';
+SELECT 'RELEASE DATE AFTER 1991' AS 'ALBUMS WITH A';
+SELECT * FROM albums WHERE release_date > 1991;
+SELECT 'THE DISCO GENRE' AS 'ALBUMS WITH';
+SELECT * FROM albums WHERE genre = 'disco';
+SELECT 'ARTIST "Various artists"' AS 'ALBUMS BY THE';
+SELECT * FROM albums WHERE artist = 'Various artists';

33ddb0bb5a4b5cded6ecfd04325a6029c33b36ce
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-27 11:50:01 -0500

    feat: add todo's for exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,2 +1,14 @@
 USE codeup_test_db;
 
+-- Write queries to find the following info, before each item output a caption explaining the results
+-- name of albums by Pink Floyd
+
+-- year Sgt. Pepper's Lonely Hearts Club Band was released
+
+-- genre for Nevermind
+
+-- albums released in 1990's
+
+-- albums less than 20mill sales
+
+-- albums with genre "Rock"

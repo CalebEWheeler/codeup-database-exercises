53c5a4778d1f12a5a0f741f0f3b31d1a9d6afc16
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-27 12:42:40 -0500

    feat: create SELECT that will display all albums by Pink Floyd

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,14 +1,26 @@
 USE codeup_test_db;
 
 -- Write queries to find the following info, before each item output a caption explaining the results
+
 -- name of albums by Pink Floyd
+SELECT 'ALBUMS BY PINK FLOYD' AS 'INFO';
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
 
 -- year Sgt. Pepper's Lonely Hearts Club Band was released
+-- SELECT 'SGT. PEPPERS LONELY HEART''S ClUB BAND' AS 'YEAR RELEASED';
+-- SELECT release_date FROM albums WHERE `name` = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 -- genre for Nevermind
+-- SELECT 'NEVERMIND' AS 'GENRE FOR';
+-- SELECT genre FROM albums WHERE `name` = 'Nevermind';
 
 -- albums released in 1990's
+-- SELECT 'RELEASED' AS 'ALBUMS FROM 1990''s';
+-- SELECT name FROM albums WHERE release_date > 1989 && release_date < 2000;
 
 -- albums less than 20mill sales
+-- SELECT 'LESS THAN 20 MILLION' AS 'ALBUM SALES';
+-- SELECT name FROM albums WHERE sales < 20;
 
 -- albums with genre "Rock"
+-- SELECT 'Rock' AS 'Genre';

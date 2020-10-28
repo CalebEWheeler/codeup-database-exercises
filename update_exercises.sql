c293cd97f8ff96d1bad20c66b03442f08792e158
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-28 09:22:52 -0500

    feat: create where_exercises.sql that uses employees database

--- update_exercises.sql
+++ update_exercises.sql
@@ -5,7 +5,7 @@ USE codeup_test_db;
 -- SELECT name FROM albums;
 -- SELECT * FROM albums;
 -- UPDATE albums
--- SET sales = sales * 10;
+-- SET sales = (sales * 10);
 -- SELECT 'SALES MULTIPLIED' AS 'BY 10';
 -- SELECT * FROM albums;
 
@@ -13,7 +13,7 @@ USE codeup_test_db;
 -- SELECT 'ALBUMS RELEASED' AS 'BEFORE 1980';
 -- SELECT name FROM albums WHERE release_date < 1980;
 -- UPDATE albums
--- SET release_date = release_date - 100
+-- SET release_date = (release_date - 100)
 -- WHERE release_date < 1980;
 -- SELECT * FROM albums;
 

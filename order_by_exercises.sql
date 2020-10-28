42609f5ffd85a90960e8b36771e57b3a918ec7eb
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-28 11:52:45 -0500

    feat: modify query that checks for employees with an 'E' in their last name and sort the results by employee number

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,9 +1,9 @@
 USE employees;
 
-SELECT *
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY last_name ASC, first_name ASC;
+-- SELECT *
+-- FROM employees
+-- WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+-- ORDER BY last_name ASC, first_name ASC;
 
 -- SELECT *
 -- FROM employees
@@ -19,10 +19,11 @@ ORDER BY last_name ASC, first_name ASC;
 -- WHERE last_name LIKE 'E%'
 --    OR last_name LIKE '%E';
 
--- SELECT *
--- FROM employees
--- WHERE last_name LIKE 'E%'
---    AND last_name LIKE '%E';
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E'
+ORDER BY emp_no ASC;
 
 -- SELECT *
 -- FROM employees

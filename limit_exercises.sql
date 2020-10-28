2196a94cdbeec4cb7289396135818eb6a679b2a0
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-28 12:05:21 -0500

    feat: modify query that finds all employees hired in the 90's and was born on christmas day in order by oldest employee hired last LIMIT 5

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,6 +1,13 @@
 USE employees;
 
-SELECT DISTINCT last_name
+-- SELECT DISTINCT last_name
+-- FROM employees
+-- ORDER BY last_name DESC
+-- LIMIT 10;
+
+SELECT *
 FROM employees
-ORDER BY last_name DESC
-LIMIT 10;
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+  AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;

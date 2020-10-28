62bf1559ee5005e5e09ec355af7c33bdb3181d89
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-28 10:11:14 -0500

    feat: create query in where_exercises.sql that will select all employees with a birthday on christmas

--- where_exercises.sql
+++ where_exercises.sql
@@ -8,6 +8,10 @@ USE employees;
 -- FROM employees
 -- WHERE last_name LIKE 'E%';
 
+-- SELECT *
+-- FROM employees
+-- WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE birth_date LIKE '%-12-25';

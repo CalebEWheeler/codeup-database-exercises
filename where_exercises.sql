ea0922a4fdad4bfb30a83b1d4f3e571fde26eb58
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-28 09:38:12 -0500

    feat: create query in where_exercises.sql that will select all employees with a last name that starts with 'E'

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,5 +1,9 @@
 USE employees;
 
+-- SELECT *
+-- FROM employees
+-- WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
 SELECT *
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE last_name LIKE 'E%';

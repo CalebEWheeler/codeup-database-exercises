68febfe650802cdfc52c14539b6d554ca113e200
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-28 10:00:59 -0500

    feat: create query in where_exercises.sql that will select all employees with a hire_date between 1990 and 1999

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,6 +4,10 @@ USE employees;
 -- FROM employees
 -- WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
+-- SELECT *
+-- FROM employees
+-- WHERE last_name LIKE 'E%';
+
 SELECT *
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';

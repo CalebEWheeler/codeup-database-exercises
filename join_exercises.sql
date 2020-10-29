fadaa4e4f761f8f1bcf66385bd6b84c700df8f72
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-29 15:06:52 -0500

    feat: create query to select current managers who are female and the departments they work in

--- join_exercises.sql
+++ join_exercises.sql
@@ -8,7 +8,7 @@ JOIN departments as d
   ON d.dept_no = de.dept_no
 WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
 
-SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ',e.last_name) AS 'Department Manager'
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ',e.last_name) AS 'Manager Name'
 FROM employees AS e
 JOIN dept_manager AS dm
      ON dm.emp_no = e.emp_no
@@ -17,12 +17,5 @@ JOIN departments AS d
 WHERE dm.to_date = '9999-01-01'
 ORDER BY d.dept_name ASC;
 
-SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ',e.last_name) AS 'Department Manager'
-FROM employees AS e
-         JOIN dept_manager AS dm
-              ON dm.emp_no = e.emp_no
-        AND e.gender = 'f'
-         JOIN departments AS d
-              ON d.dept_no = dm.dept_no
-WHERE dm.to_date = '9999-01-01'
-ORDER BY d.dept_name ASC;
+
+

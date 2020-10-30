4bb422aa017f5ef4dceacd0cf20df1ea7a84652c
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-30 10:23:12 -0500

    feat: create query that will display all DISTINCT employee last names that start and end with 'E'

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,4 +1,9 @@
 USE employees;
 
 SELECT DISTINCT t.title
-FROM titles AS t
+FROM titles AS t;
+
+SELECT e.last_name
+FROM employees AS e
+WHERE e.last_name LIKE 'E%' AND e.last_name LIKE '%E'
+GROUP BY e.last_name;

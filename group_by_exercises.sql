d3183d390622cd5080e933809ef35a78c80d5d06
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-30 10:29:38 -0500

    feat: create query that will display DISTINCT last names that have a 'q' in the name, but not 'qu'

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -12,3 +12,8 @@ SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'FULL NAME'
 FROM employees AS e
 WHERE e.last_name LIKE 'E%' AND e.last_name LIKE '%E'
 GROUP BY CONCAT(e.first_name, ' ', e.last_name)
+
+SELECT e.last_name
+FROM employees AS e
+WHERE e.last_name LIKE '%q%' AND e.last_name NOT LIKE '%qu%'
+GROUP BY e.last_name;

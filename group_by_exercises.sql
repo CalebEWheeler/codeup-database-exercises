4bc7b7c85d2caa1a65ba193ebd45913492cb8477
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-30 10:34:13 -0500

    feat: add count() to the previous results and used ORDER BY to display the unusual names in order from least amount of employees with the specified last name to the greatest amount

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -11,9 +11,12 @@ GROUP BY e.last_name;
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'FULL NAME'
 FROM employees AS e
 WHERE e.last_name LIKE 'E%' AND e.last_name LIKE '%E'
-GROUP BY CONCAT(e.first_name, ' ', e.last_name)
+GROUP BY CONCAT(e.first_name, ' ', e.last_name);
 
-SELECT e.last_name
+SELECT e.last_name AS LastNAME, COUNT(*) AS Count
 FROM employees AS e
 WHERE e.last_name LIKE '%q%' AND e.last_name NOT LIKE '%qu%'
-GROUP BY e.last_name;
+GROUP BY e.last_name
+ORDER BY COUNT(*);
+
+

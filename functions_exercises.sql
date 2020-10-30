7a02974e355457ff0dfad7336286731898aaca07
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-30 09:58:49 -0500

    feat: copy and refactor query to get all employees born on Christmas and hired in the 90's and create a new table that will show each employees full name and show how many days they have worked with the company

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -19,28 +19,34 @@ USE employees;
 -- WHERE last_name LIKE 'E%'
 --    OR last_name LIKE '%E';
 
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%'
-  AND last_name LIKE '%E'
-ORDER BY emp_no DESC;
-
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'FULL NAME'
-FROM employees AS e
-WHERE last_name LIKE 'E%'
-  AND last_name LIKE '%E'
-ORDER BY emp_no DESC;
+# SELECT *
+# FROM employees
+# WHERE last_name LIKE 'E%'
+#   AND last_name LIKE '%E'
+# ORDER BY emp_no DESC;
+#
+# SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'FULL NAME'
+# FROM employees AS e
+# WHERE last_name LIKE 'E%'
+#   AND last_name LIKE '%E'
+# ORDER BY emp_no DESC;
 
 -- SELECT *
 -- FROM employees
 -- WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
--- SELECT *
--- FROM employees
--- WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
---   AND birth_date LIKE '%-12-25'
--- ORDER BY birth_date ASC, hire_date DESC
+# SELECT *
+# FROM employees
+# WHERE hire_date LIKE '199%'
+#   AND birth_date LIKE '%-12-25'
+# ORDER BY birth_date ASC, hire_date DESC
 -- LIMIT 5 OFFSET 45;
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'FULL NAME', datediff(NOW(), e.hire_date) AS 'DAYS WITH COMPANY'
+FROM employees AS e
+WHERE hire_date LIKE '199%'
+  AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
 
 -- SELECT *
 -- FROM employees

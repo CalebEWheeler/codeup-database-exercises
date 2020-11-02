93eaa8ed85623e71350580dadd9a8ad65eaa5d40
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-11-02 14:23:22 -0600

    feat: refactor ad_lister.sql to simplfy tables. Removed user_ad table and created SELECT queries to properly receive the proper information

--- ad_lister.sql
+++ ad_lister.sql
@@ -22,20 +22,20 @@ CREATE TABLE ad
     id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
     title       VARCHAR(50),
     description TEXT,
-    ad_id INT UNSIGNED,
+    user_id INT UNSIGNED,
     PRIMARY KEY (id),
-    FOREIGN KEY (ad_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
-INSERT INTO ad(title, description)
-VALUES ('ASUS monitor', 'ASUS 24" gaming monitor'),
-       ('5" custom exhaust tip', 'custom made powder-coated 5" exhaust tip with 4" inlet'),
-       ('lawn mower', 'husquevarna lawn mower build date 2012 - lightly used'),
-       ('weed eater', 'stihl battery operated weed eater'),
-       ('Dyson V8', 'Dyson animal V8 - comes with all attachments and manufacturer information'),
-       ('Hand Washing Set', 'lightly used hand washing drying rack with scrub brushes for tall glasses and a 6pack of steel wool'),
-       ('Logitech Keyboard', 'Logitech gaming rgb keyboard'),
-       ('Kenwood head unit', 'double din 2019 Kenwood stereo with touch screen');
+INSERT INTO ad(title, description, user_id)
+VALUES ('ASUS monitor', 'ASUS 24" gaming monitor', 1),
+       ('5" custom exhaust tip', 'custom made powder-coated 5" exhaust tip with 4" inlet', 1),
+       ('lawn mower', 'husquevarna lawn mower build date 2012 - lightly used', 2),
+       ('weed eater', 'stihl battery operated weed eater', 2),
+       ('Dyson V8', 'Dyson animal V8 - comes with all attachments and manufacturer information', 4),
+       ('Hand Washing Set', 'lightly used hand washing drying rack with scrub brushes for tall glasses and a 6pack of steel wool', 4),
+       ('Logitech Keyboard', 'Logitech gaming rgb keyboard', 3),
+       ('Kenwood head unit', 'double din 2019 Kenwood stereo with touch screen', 3);
 
 CREATE TABLE category
 (
@@ -49,24 +49,6 @@ VALUES ('lawn equipment'),
        ('electronics'),
        ('auto parts'),
        ('home goods');
-
-CREATE TABLE user_ad
-(
-    user_id INTEGER UNSIGNED NOT NULL,
-    ad_id   INTEGER UNSIGNED NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user (id),
-    FOREIGN KEY (ad_id) REFERENCES ad (id)
-);
-
-INSERT INTO user_ad
-VALUES (1, 1),
-       (1, 2),
-       (2, 3),
-       (2, 4),
-       (4, 7),
-       (4, 8),
-       (3, 5),
-       (3, 6);
 
 CREATE TABLE ad_category
 (
@@ -88,3 +70,25 @@ VALUES (1, 2),
        (7, 2),
        (8, 2),
        (8, 3);
+
+SELECT email
+FROM user AS u
+         JOIN ad ON ad.user_id = u.id
+WHERE ad.id = '7';
+
+SELECT name
+FROM category AS c
+         JOIN ad_category AS adc ON adc.category_id = c.id
+         JOIN ad ON ad.id = adc.ad_id
+WHERE ad.id = '4';
+
+SELECT *
+FROM ad
+         JOIN ad_category AS adc ON adc.ad_id = ad.id
+         JOIN category AS c ON c.id = adc.category_id
+WHERE c.id = '4';
+
+
+SELECT *
+FROM ad
+WHERE user_id = '2';
